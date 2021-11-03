4fc091885ed0d1dbe1aeb19a5549654b07d5eae7
Nikolay Romanchenko <romanchenko2804@gmail.com>
2021-11-03 21:34:59 +0200

    task#1

---
+++ js/task-01.js
@@ -0,0 +1,8 @@
+const countOfCategoriesRef = document.querySelectorAll('.item');
+console.log(`Number of categories: `, countOfCategoriesRef.length);
+
+const categoryRef = document.querySelectorAll('.item h2');
+const nameOfCategoryRef = categoryRef.forEach((cat) => {
+    console.log(`Category: `, cat.textContent);
+    console.log(`Elements: `, cat.nextElementSibling.children.length);
+});
