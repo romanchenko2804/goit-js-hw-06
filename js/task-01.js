70bd7f84b87b39ce325e268522746d080130dfb3
Nikolay Romanchenko <romanchenko2804@gmail.com>
2021-11-03 22:10:43 +0200

    task#2 - done

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,8 @@
-const countOfCategoriesRef = document.querySelectorAll('.item');
+const countOfCategoriesRef = document.querySelectorAll(".item");
 console.log(`Number of categories: `, countOfCategoriesRef.length);
 
-const categoryRef = document.querySelectorAll('.item h2');
+const categoryRef = document.querySelectorAll(".item h2");
 const nameOfCategoryRef = categoryRef.forEach((cat) => {
-    console.log(`Category: `, cat.textContent);
-    console.log(`Elements: `, cat.nextElementSibling.children.length);
+  console.log(`Category: `, cat.textContent);
+  console.log(`Elements: `, cat.nextElementSibling.children.length);
 });
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredient = ingredients.map((element) => {
+  const listItemRef = document.createElement("li");
+  listItemRef.textContent = element;
+  listItemRef.classList.add("item");
+
+  return listItemRef;
+});
+
+const listRef = document.querySelector("#ingredients");
+listRef.append(...ingredient);
